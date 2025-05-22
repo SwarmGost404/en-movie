4fc94f8bc006e2c1c812c8dbbfd26f306daba90f
SwarmGost404 <vik.z.c.r@gmail.com>
2025-05-22 17:35:41 -0500

    install

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
+   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
